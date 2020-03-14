8b5b051aa5c8c8b53cc8157a216e7f954d6dbe08
Łukasz <riraseth@gmail.com>
2020-03-14 20:00:57 +0100

    neon scroll bar

---
+++ neon_scroll/script.js
@@ -0,0 +1,6 @@
+const progressbar = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progressbar.style.height = `${progressHeight}%`;
+};
